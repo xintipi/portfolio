557b6dd046b1d2c9fe4c89b05fb1b4562a52043d
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-10-25 10:49:49 +0700

    fix: fix bug

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -20,6 +20,7 @@ const Layout: FC<Props> = ({ children, title, desciption }) => {
       <Head>
         <title>{pageTitle}</title>
         <meta name="description" content={desciption} />
+        <link rel="preload" as="font" />
       </Head>
 
       <div className="flex min-h-screen flex-col">
