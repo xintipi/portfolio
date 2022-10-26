5411e697c2b3395ca91581cd0250b703ffdfd60b
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-10-26 23:17:37 +0700

    fix: fixbug

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,8 @@
-import { GetStaticProps } from 'next'
-import { FC } from 'react'
-
 import HeroSection from '@/components/partials/HeroSection'
-import { social, SocialObject } from '@/data/social'
+import { social } from '@/data/social'
 import Layout from '@/layouts/Layout'
 
-export const getStaticProps: GetStaticProps = async () => {
-  return {
-    props: {
-      social,
-    },
-  }
-}
-
-const Home: FC<{ social: SocialObject[] }> = ({ social }) => {
+const Home = () => {
   return (
     <Layout
       title="Home"
