da2f3ffb1d0255e11f4ae41990afb8e4f4ac9168
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-10-26 22:16:56 +0700

    fix: fixbug

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+import React from 'react'
+
+import { Favicon, favicon } from '@/data/favicon'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
