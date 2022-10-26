a498e6885973e5af66b011c4bdf54465c781e1ef
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-10-26 22:10:34 +0700

    fix: fixbug

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -19,7 +19,8 @@ const Home: FC<{ social: SocialObject[] }> = ({ social }) => {
       title="Home"
       desciption="Hi, I am Nguyen Huu Trung, I am a frontend developer. I work from Ho Chi Minh city, Vietnam."
       keywords="Nguyen Huu Trung, Xintipi, Frontend Developer, Mail me">
-      <HeroSection content={social} />
+      {/*<HeroSection content={social} />*/}
+      <div>Index</div>
     </Layout>
   )
 }
