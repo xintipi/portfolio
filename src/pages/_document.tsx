48d8d9b49ba5433d734ea306ce7449d4909b59ce
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-10-26 22:35:30 +0700

    fix: fixbug

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,32 +3,10 @@ import React from 'react'
 
 import { Favicon, favicon } from '@/data/favicon'
 
-export default function Document() {
+export default function Document(props: { __NEXT_DATA__: { locale: string } }) {
   return (
-    <Html lang="en">
-      <Head>
-        <meta charSet="utf-8" key="charSet" />
-        <meta name="viewport" content="width=device-width" key="viewport" />
-        <meta name="robots" content="noindex" />
-        {favicon.icon.map((link: Favicon) => (
-          <link
-            key={link.href}
-            type={link.type}
-            sizes={link.sizes}
-            rel={link.rel}
-            href={link.href}
-          />
-        ))}
-        {favicon.appleIcon.map((link: Favicon) => (
-          <link
-            key={link.href}
-            type={link.type}
-            sizes={link.sizes}
-            rel={link.rel}
-            href={link.href}
-          />
-        ))}
-      </Head>
+    <Html dir={props.__NEXT_DATA__.locale === 'en' ? 'ltr' : 'rtl'}>
+      <Head />
       <body>
         <Main />
         <NextScript />
