68968c248f397defef20e3125bb60b1a84ebaebe
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-10-26 19:55:40 +0700

    fix: fxbug

--- src/components/partials/HeroSection.tsx
+++ src/components/partials/HeroSection.tsx
@@ -2,13 +2,23 @@ import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { FiFacebook, FiGithub, FiInstagram, FiLinkedin } from 'react-icons/fi'
+import { Tooltip } from 'react-tippy'
 import Typed from 'react-typed'
 
+import { SocialObject } from '@/data/social'
 import styles from '@/styles/modules/Button.module.scss'
 
 import HeroBackground from './HeroBackground'
 
-const HeroSection = () => {
+const HeroSection = ({ content }: { content: SocialObject[] }) => {
+  const components = {
+    FiInstagram: <FiInstagram size={25} />,
+    FiFacebook: <FiFacebook size={25} />,
+    FiLinkedin: <FiLinkedin size={25} />,
+    FiGithub: <FiGithub size={25} />,
+  }
+
   return (
     <div className="hero relative -mt-16 flex items-center justify-center">
       <HeroBackground />
@@ -20,6 +30,20 @@ const HeroSection = () => {
         <div className="mt-2 flex gap-1">
           {' '}
           I'm a <Typed strings={['Frontend Developer']} typeSpeed={55} backSpeed={45} loop />
+        </div>
+        <div className="mt-4 flex gap-5">
+          {content.map((social: SocialObject) => (
+            <Tooltip key={social.title} title={social.title} trigger="mouseenter" animation="scale">
+              <a
+                href={social.href}
+                target="_blank"
+                className="cursor-pointer text-gray-700 transition-colors duration-150 hover:text-primary-500 dark:text-primary-500"
+                aria-label={social.title}
+                rel="noreferrer">
+                {components[social.icon as keyof typeof components]}
+              </a>
+            </Tooltip>
+          ))}
         </div>
         <Link href="mailto:huutrung.mmt@gmail.com" legacyBehavior>
           <a className={clsx([styles.btn, 'mt-5 px-8'])} target="_blank" rel="noreferrer">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,26 @@
 import type { NextPage } from 'next'
+import { GetStaticProps } from 'next'
 
 import HeroSection from '@/components/partials/HeroSection'
+import { social, SocialObject } from '@/data/social'
 import Layout from '@/layouts/Layout'
 
-const Home: NextPage = () => {
+const Home: NextPage<{ social: SocialObject[] }> = ({ social }) => {
   return (
     <Layout
       title="Home"
       desciption="Hi, I am Nguyen Huu Trung, I am a frontend developer. I work from Ho Chi Minh city, Vietnam.">
-      <HeroSection />
+      <HeroSection content={social} />
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      social,
+    },
+  }
 }
 
 export default Home
