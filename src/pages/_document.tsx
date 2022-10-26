287a7d4c2dbba1aa0cee0cfd2554ef2a8bba0805
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-10-26 22:47:06 +0700

    fix: fixbug

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,12 @@ export default function Document(props: { __NEXT_DATA__: { locale: string } }) {
           sizes="16x16"
           href="https://i.ibb.co/58BpJgP/favicon-16.png"
         />
+        <link
+          rel="applue-touch-icon"
+          type="image/png"
+          sizes="180x180"
+          href="https://i.ibb.co/25ZVR47/apple-touch-icon-180.png"
+        />
       </Head>
       <body>
         <Main />
