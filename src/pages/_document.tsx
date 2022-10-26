56c6378b658b56135b49a4d8f1693cdf193df382
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-10-26 22:31:18 +0700

    fix: fixbug

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,29 @@ import { Favicon, favicon } from '@/data/favicon'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" key="charSet" />
+        <meta name="viewport" content="width=device-width" key="viewport" />
+        <meta name="robots" content="noindex" />
+        {favicon.icon.map((link: Favicon) => (
+          <link
+            key={link.href}
+            type={link.type}
+            sizes={link.sizes}
+            rel={link.rel}
+            href={link.href}
+          />
+        ))}
+        {favicon.appleIcon.map((link: Favicon) => (
+          <link
+            key={link.href}
+            type={link.type}
+            sizes={link.sizes}
+            rel={link.rel}
+            href={link.href}
+          />
+        ))}
+      </Head>
       <body>
         <Main />
         <NextScript />
