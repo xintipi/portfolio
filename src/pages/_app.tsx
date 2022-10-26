a468ac561cad2151ab055af6aed662f1d39af139
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-10-26 22:13:53 +0700

    fix: fixbug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,29 +14,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store as Store}>
       <ThemeProvider>
-        <Head>
-          <meta charSet="utf-8" key="charSet" />
-          <meta name="viewport" content="width=device-width" key="viewport" />
-          <meta name="robots" content="noindex" />
-          {favicon.icon.map((link: Favicon) => (
-            <link
-              key={link.href}
-              type={link.type}
-              sizes={link.sizes}
-              rel={link.rel}
-              href={link.href}
-            />
-          ))}
-          {favicon.appleIcon.map((link: Favicon) => (
-            <link
-              key={link.href}
-              type={link.type}
-              sizes={link.sizes}
-              rel={link.rel}
-              href={link.href}
-            />
-          ))}
-        </Head>
         <Component {...pageProps} />
       </ThemeProvider>
     </Provider>
