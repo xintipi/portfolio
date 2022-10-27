f84548d7220b6c3ce870f8bb767a8ea9c91c8fd2
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-10-27 16:20:43 +0700

    feat: next seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,41 +20,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <Provider store={store as Store}>
-      <NextSeo
-        openGraph={{
-          url: process.env.NEXT_PUBLIC_DOMAIN,
-          locale: lang === 'en' ? 'en_US' : 'vi_VN',
-          images: [
-            {
-              url: process.env.NEXT_PUBLIC_DOMAIN + 'images/avatar/me.webp',
-              width: 1024,
-              height: 1024,
-              alt: 'Nguyen Huu Trung',
-              type: 'image/webp',
-            },
-          ],
-        }}
-        additionalLinkTags={[
-          {
-            rel: 'icon',
-            type: 'image/png',
-            sizes: '16x16',
-            href: 'https://i.ibb.co/58BpJgP/favicon-16.png',
-          },
-          {
-            rel: 'icon',
-            type: 'image/png',
-            sizes: '32x32',
-            href: 'https://i.ibb.co/VqNBMxR/favicon-32.png',
-          },
-          {
-            rel: 'applue-touch-icon',
-            type: 'image/png',
-            sizes: '180x180',
-            href: 'https://i.ibb.co/25ZVR47/apple-touch-icon-180.png',
-          },
-        ]}
-      />
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
