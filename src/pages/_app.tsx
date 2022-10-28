790c125490282f6b6dd0c73193cadccf92302e2b
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-10-28 14:59:56 +0700

    chore: change domain

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ import { Store, store } from '@/store'
 import * as ga from '../lib/ga'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { query, pathname, events } = useRouter()
+  const { query, events } = useRouter()
   const lang = query.lang === 'vi' ? 'vi' : 'en'
 
   useEffect(() => {
@@ -32,36 +32,8 @@ function MyApp({ Component, pageProps }: AppProps) {
     }
   }, [events])
 
-  const openGraph = useMemo(() => {
-    return {
-      url: process.env.NEXT_PUBLIC_DOMAIN,
-      description: 'Frontend Developer, UI Designer, and Open Source Advocate.',
-      locale: lang === 'en' ? 'en_US' : 'vi_VN',
-      images: [
-        {
-          url: process.env.NEXT_PUBLIC_DOMAIN + 'images/avatar/me.webp',
-          width: 1024,
-          height: 1024,
-          alt: 'Nguyen Huu Trung',
-          type: 'image/webp',
-        },
-      ],
-    }
-  }, [pathname])
-
   return (
     <Provider store={store as Store}>
-      <NextSeo openGraph={pathname !== '/blog/[slug]' ? openGraph : {}} />
-      <SocialProfileJsonLd
-        type="Person"
-        name="Nguyen Huu Trung"
-        url={process.env.NEXT_PUBLIC_DOMAIN as string}
-        sameAs={[
-          'https://www.facebook.com/huutrung.mmt',
-          'https://www.instagram.com/n.h.trung_xinn/',
-          'https://www.linkedin.com/in/nguy%E1%BB%85n-h%E1%BB%AFu-trung-75859621a/',
-        ]}
-      />
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from 'next'
 import { SocialProfileJsonLd } from 'next-seo'
-import { FC, Fragment } from 'react'
+import React, { FC, Fragment, useMemo } from 'react'
 
 import AboutSection from '@/components/partials/AboutSection'
 import HeroSection from '@/components/partials/HeroSection'
@@ -16,13 +16,45 @@ type Props = {
 }
 
 const Home: FC<Props> = ({ socials, about }) => {
+  const openGraph = useMemo(() => {
+    return {
+      title: 'XIN - Developer',
+      description: 'Frontend Developer, UI Designer, and Open Source Advocate.',
+      url: process.env.NEXT_PUBLIC_DOMAIN,
+      type: 'profile',
+      profile: {
+        firstName: 'Trung',
+        lastName: 'Nguyen Huu',
+        gender: 'male',
+      },
+      images: [
+        {
+          url: process.env.NEXT_PUBLIC_DOMAIN + 'images/avatar/me.webp',
+          width: 1024,
+          height: 1024,
+          alt: 'Profile photo',
+        },
+      ],
+    }
+  }, [])
   return (
     <Fragment>
+      <SocialProfileJsonLd
+        type="Person"
+        name="Nguyen Huu Trung"
+        url={process.env.NEXT_PUBLIC_DOMAIN as string}
+        sameAs={[
+          'https://www.facebook.com/huutrung.mmt',
+          'https://www.instagram.com/n.h.trung_xinn/',
+          'https://www.linkedin.com/in/nguy%E1%BB%85n-h%E1%BB%AFu-trung-75859621a/',
+        ]}
+      />
+
       <Layout
         title="XIN - Developer"
-        description="Frontend Developer, UI Designer, and Open Source Advocate.">
+        description="Frontend Developer, UI Designer, and Open Source Advocate."
+        openGraph={openGraph}>
         <HeroSection socials={socials} />
-
         <section className="container pt-20 pb-10">
           <AboutSection about={about[0].description} />
         </section>
