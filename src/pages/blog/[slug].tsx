7f702ca03d38427bb6d996f92909472b8bf79134
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-10-28 17:09:38 +0700

    chore: change domain

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -40,21 +40,21 @@ const BlogSingle: FC<Props> = ({ post, host }) => {
 
   return (
     <Fragment>
-      <ArticleJsonLd
-        key={`blogJSON-${post.id}`}
-        type="BlogPosting"
-        url="https://example.com/blog"
-        title="Blog headline"
-        images={[
-          'https://example.com/photos/1x1/photo.jpg',
-          'https://example.com/photos/4x3/photo.jpg',
-          'https://example.com/photos/16x9/photo.jpg',
-        ]}
-        datePublished="2015-02-05T08:00:00+08:00"
-        dateModified="2015-02-05T09:00:00+08:00"
-        authorName="Jane Blogs"
-        description="This is a mighty good description of this blog."
-      />
+      {/*<ArticleJsonLd*/}
+      {/*  key={`blogJSON-${post.id}`}*/}
+      {/*  type="BlogPosting"*/}
+      {/*  url="https://example.com/blog"*/}
+      {/*  title="Blog headline"*/}
+      {/*  images={[*/}
+      {/*    'https://example.com/photos/1x1/photo.jpg',*/}
+      {/*    'https://example.com/photos/4x3/photo.jpg',*/}
+      {/*    'https://example.com/photos/16x9/photo.jpg',*/}
+      {/*  ]}*/}
+      {/*  datePublished="2015-02-05T08:00:00+08:00"*/}
+      {/*  dateModified="2015-02-05T09:00:00+08:00"*/}
+      {/*  authorName="Jane Blogs"*/}
+      {/*  description="This is a mighty good description of this blog."*/}
+      {/*/>*/}
 
       <Layout
         title="Blog"
