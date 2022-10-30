5f7b247d5ac9acab00472277d509df1d6069a434
Nguyen Huu Trung <huutrung.mmt@gmail.com>
2022-10-30 12:34:24 +0700

    fix: fix minor bugs

--- src/pages/server-sitemap.xml/index.tsx
+++ src/pages/server-sitemap.xml/index.tsx
@@ -10,21 +10,19 @@ export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSideP
   // Method to source urls from cms
   // const urls = await fetch('https//example.com/api')
 
-  const fieldPosts: ISitemapField[] = posts.map((item) => ({
+  const fields: ISitemapField[] = posts.map((item) => ({
     loc: `${process.env.NEXT_PUBLIC_DOMAIN}blog/${item.slug}`,
     lastmod: new Date().toISOString(),
-    priority: 0.7,
-    changefreq: 'daily',
   }))
 
-  const fieldWorks: ISitemapField[] = works.map((item) => ({
-    loc: `${process.env.NEXT_PUBLIC_DOMAIN}works/${item.slug}`,
-    lastmod: new Date().toISOString(),
-    priority: 0.7,
-    changefreq: 'daily',
-  }))
+  works.map((item) => {
+    fields.push({
+      loc: `${process.env.NEXT_PUBLIC_DOMAIN}works/${item.slug}`,
+      lastmod: new Date().toISOString(),
+    })
+  })
 
-  return getServerSideSitemap(ctx, [...fieldPosts, ...fieldWorks])
+  return getServerSideSitemap(ctx, fields)
 }
 
 // Default export to prevent next.js errors
