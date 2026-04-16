a1af53ad95bccad659c1a5da6b518c9e8aa1fb88
trung-nextgen <trung@nextgeneration.io>
2026-04-16 11:39:09 +0700

    chore: update project name and fix CV download link in About component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,18 +1,6 @@
-import React from 'react';
 import './About.css';
 import logo from '../../assets/me.jpg';
 import AboutBox from './AboutBox';
-
-const skillsList = [
-	{
-		id: 1,
-		name: 'Development',
-		class: 'development',
-		number: '90%',
-	},
-	{ id: 2, name: 'UI/UX design', class: 'ui__design', number: '70%' },
-	{ id: 3, name: 'Wordpress', class: 'wordpress', number: '80%' },
-];
 
 const About = () => {
 	return (
@@ -22,7 +10,7 @@ const About = () => {
 			<div className="about__container grid">
 				<div className="about__image__Wrapper">
 					<img src={logo} alt="" className="about__img" />
-					<a href="https://raw.githubusercontent.com/foverokavindz/fovero-folio/main/public/mycv.pdf" download="resume" className="btn">
+					<a href="/mycv.pdf" download="resume" className="btn">
 						<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{ verticalAlign: 'middle', marginRight: '15px' }}>
 							<path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
 							<polyline points="7 10 12 15 17 10" />
